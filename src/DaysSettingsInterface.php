d603d6b7eb0ca484f8af6501e3d878ac045d8e8b
noumecha spaker <noumechaivan@gmail.com>
2023-01-11 15:47:02 +0100

    maj : System Form managment

--- src/DaysSettingsInterface.php
+++ src/DaysSettingsInterface.php
@@ -13,81 +13,281 @@ namespace Drupal\booking_system;
  * Defines an interface for days configuration.
  */
 interface DaysSettingsInterface /*extends PluginInspectionInterface*/ {
-  const booking_type = [
-    [
-      'label' => 'déjeuner',
-      'status' => 'false',
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
-    ],
-    [
-      'label' => 'diné',
-      'status' => 'false',
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
-    ]
-  ];
   const jours = [
     [
       'label' => 'Dimanche',
-      'status' => false,
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
+      'status' => false, // status pour le jour
+      'periodes' => [
+        [
+        'label' => 'Petit déjeuné',
+        'status' => true, // status pour période
+        'h_d' => 07,
+        'm_d' => 00,
+        'h_f' => 9,
+        'm_f' => 00,
+        'decallage' => 5,
+        'intervalle' => 15,
+        'creneaux' => []
+        ],
+        [
+          'label' => 'Déjeuner',
+          'status' => true, // status pour période
+          'h_d' => 12,
+          'm_d' => 00,
+          'h_f' => 14,
+          'm_f' => 00,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ],
+        [
+          'label' => 'Diner',
+          'status' => true, // status pour période
+          'h_d' => 17,
+          'm_d' => 00,
+          'h_f' => 19,
+          'm_f' => 30,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ]
+      ]
     ],
     [
       'label' => 'Lundi',
-      'status' => true,
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
+      'text' => 'noumel',
+      'status' => false, // status pour le jour
+      'periodes' => [
+        [
+        'label' => 'Petit déjeuné',
+        'status' => true, // status pour période
+        'h_d' => 07,
+        'm_d' => 00,
+        'h_f' => 9,
+        'm_f' => 00,
+        'decallage' => 5,
+        'intervalle' => 15,
+        'creneaux' => []
+        ],
+        [
+          'label' => 'Déjeuner',
+          'status' => true, // status pour période
+          'h_d' => 12,
+          'm_d' => 00,
+          'h_f' => 14,
+          'm_f' => 00,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ],
+        [
+          'label' => 'Diner',
+          'status' => true, // status pour période
+          'h_d' => 17,
+          'm_d' => 00,
+          'h_f' => 19,
+          'm_f' => 30,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ]
+      ]
     ],
     [
       'label' => 'Mardi',
-      'status' => true,
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
+      'status' => false, // status pour le jour
+      'periodes' => [
+        [
+        'label' => 'Petit déjeuné',
+        'status' => true, // status pour période
+        'h_d' => 07,
+        'm_d' => 00,
+        'h_f' => 9,
+        'm_f' => 00,
+        'decallage' => 5,
+        'intervalle' => 15,
+        'creneaux' => []
+        ],
+        [
+          'label' => 'Déjeuner',
+          'status' => true, // status pour période
+          'h_d' => 12,
+          'm_d' => 00,
+          'h_f' => 14,
+          'm_f' => 00,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ],
+        [
+          'label' => 'Diner',
+          'status' => true, // status pour période
+          'h_d' => 17,
+          'm_d' => 00,
+          'h_f' => 19,
+          'm_f' => 30,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ]
+      ]
     ],
     [
       'label' => 'Mercredi',
-      'status' => true,
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
+      'status' => false, // status pour le jour
+      'periodes' => [
+        [
+        'label' => 'Petit déjeuné',
+        'status' => true, // status pour période
+        'h_d' => 07,
+        'm_d' => 00,
+        'h_f' => 9,
+        'm_f' => 00,
+        'decallage' => 5,
+        'intervalle' => 15,
+        'creneaux' => []
+        ],
+        [
+          'label' => 'Déjeuner',
+          'status' => true, // status pour période
+          'h_d' => 12,
+          'm_d' => 00,
+          'h_f' => 14,
+          'm_f' => 00,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ],
+        [
+          'label' => 'Diner',
+          'status' => true, // status pour période
+          'h_d' => 17,
+          'm_d' => 00,
+          'h_f' => 19,
+          'm_f' => 30,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ]
+      ]
     ],
     [
       'label' => 'Jeudi',
-      'status' => true,
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
+      'status' => false, // status pour le jour
+      'periodes' => [
+        [
+        'label' => 'Petit déjeuné',
+        'status' => true, // status pour période
+        'h_d' => 07,
+        'm_d' => 00,
+        'h_f' => 9,
+        'm_f' => 00,
+        'decallage' => 5,
+        'intervalle' => 15,
+        'creneaux' => []
+        ],
+        [
+          'label' => 'Déjeuner',
+          'status' => true, // status pour période
+          'h_d' => 12,
+          'm_d' => 00,
+          'h_f' => 14,
+          'm_f' => 00,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ],
+        [
+          'label' => 'Diner',
+          'status' => true, // status pour période
+          'h_d' => 17,
+          'm_d' => 00,
+          'h_f' => 19,
+          'm_f' => 30,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ]
+      ]
     ],
     [
       'label' => 'Vendredi',
-      'status' => true,
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
+      'status' => false, // status pour le jour
+      'periodes' => [
+        [
+        'label' => 'Petit déjeuné',
+        'status' => true, // status pour période
+        'h_d' => 07,
+        'm_d' => 00,
+        'h_f' => 9,
+        'm_f' => 00,
+        'decallage' => 5,
+        'intervalle' => 15,
+        'creneaux' => []
+        ],
+        [
+          'label' => 'Déjeuner',
+          'status' => true, // status pour période
+          'h_d' => 12,
+          'm_d' => 00,
+          'h_f' => 14,
+          'm_f' => 00,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ],
+        [
+          'label' => 'Diner',
+          'status' => true, // status pour période
+          'h_d' => 17,
+          'm_d' => 00,
+          'h_f' => 19,
+          'm_f' => 30,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ]
+      ]
     ],
     [
       'label' => 'Samedi',
-      'status' => false,
-      'h_d' => 7,
-      'm_d' => 0,
-      'h_f' => 17,
-      'm_f' => 0
-    ]
+      'status' => false, // status pour le jour
+      'periodes' => [
+        [
+        'label' => 'Petit déjeuné',
+        'status' => true, // status pour période
+        'h_d' => 07,
+        'm_d' => 00,
+        'h_f' => 9,
+        'm_f' => 00,
+        'decallage' => 5,
+        'intervalle' => 15,
+        'creneaux' => []
+        ],
+        [
+          'label' => 'Déjeuner',
+          'status' => true, // status pour période
+          'h_d' => 12,
+          'm_d' => 00,
+          'h_f' => 14,
+          'm_f' => 00,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ],
+        [
+          'label' => 'Diner',
+          'status' => true, // status pour période
+          'h_d' => 17,
+          'm_d' => 00,
+          'h_f' => 19,
+          'm_f' => 30,
+          'decallage' => 10,
+          'intervalle' => 30,
+          'creneaux' => []
+        ]
+      ]
+    ],
   ];
 
 }
--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -29,114 +29,103 @@ class SettingsForm extends ConfigFormBase implements DaysSettingsInterface {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('booking_system.settings');
-    /*$form['example'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->config('booking_system.settings')->get('example'),
-    ];*/
-    $form['id'] = [
-      '#type' => 'machine_name',
-      //'#default_value' => $manage_days_entity_type->id(),
-      '#default_value' => $this->config('booking_system.settings')->get('id'),
-      '#machine_name' => [
-        'exists' => '\Drupal\booking_system\Entity\ManageDaysEntityType::load'
-      ],
-      //'#disabled' => !$manage_days_entity_type->isNew()
-    ];
-
-    // dinner type : 
-    $booking_type = \Drupal\booking_system\DaysSettingsInterface::booking_type;
-    if (!empty($this->config('booking_system.settings')->get('booking_type'))) {
-      $booking_type = $this->config('booking_system.settings')->get('booking_type');
-    }
-    $form['booking_type'] = [
-      '#type' => 'fieldset',
-      '#title' => 'Configuration Heure de debut & fin des types',
-      '#tree' => TRUE
-    ];
-    foreach ($booking_type as $i => $val) {
-      //
-      $form['booking_type'][$i] = [
-        "#type" => 'details',
-        '#title' => $val['label'],
-        '#open' => false
-      ];
-      $form['booking_type'][$i]['label'] = [
-        "#type" => 'textfield',
-        '#title' => 'Label',
-        '#default_value' => $val['label']
-      ];
-      //
-      $form['booking_type'][$i]['status'] = [
-        "#type" => 'checkbox',
-        '#title' => 'Status',
-        '#default_value' => $val['status']
-      ];
-      //
-      $form['booking_type'][$i]['h_d__m_d'] = [
-        "#type" => 'textfield',
-        '#title' => 'Heure debut',
-        '#default_value' => $val['h_d'] . ':' . $val['m_d']
-      ];
-      //
-      $form['booking_type'][$i]['h_f__m_f'] = [
-        "#type" => 'textfield',
-        '#title' => 'Heure fin',
-        '#default_value' => $val['h_f'] . ':' . $val['m_f']
-      ];
-    }
+    $config = $this->config('booking_system.settings')->getRawData();
+    dump($config);
     /* You will need additional form elements for your custom properties. */
     $jours = \Drupal\booking_system\DaysSettingsInterface::jours;
-    if (!empty($this->config('booking_system.settings')->get('jours'))) {
+    //dump($jours);
+    /*if (!empty($this->config('booking_system.settings')->get('jours'))) {
       $jours = $this->config('booking_system.settings')->get('jours');
-    }
+    }*/
     $form['jours'] = [
       '#type' => 'fieldset',
       '#title' => 'Configuration des dates',
       '#tree' => TRUE
     ];
-    //
+    // boucles pour l'affichage des élemntes des jours
     foreach ($jours as $i => $val) {
-      //
+      //dump($jours);
+      // display the dropdown
       $form['jours'][$i] = [
         "#type" => 'details',
         '#title' => $val['label'],
         '#open' => false
       ];
+      // label
       $form['jours'][$i]['label'] = [
         "#type" => 'textfield',
         '#title' => 'Label',
         '#default_value' => $val['label']
       ];
-      //
+      // status
       $form['jours'][$i]['status'] = [
         "#type" => 'checkbox',
         '#title' => 'Status',
-        '#default_value' => $val['status']
+        '#default_value' => isset($config['jours'][$i]['status']) ? $config['jours'][$i]['status'] : $val['status']
       ];
-      //
-      $form['jours'][$i]['h_d__m_d'] = [
-        "#type" => 'textfield',
-        '#title' => 'Heure debut',
-        '#default_value' => $val['h_d'] . ':' . $val['m_d']
+      // periodes
+      $form['jours'][$i]['periodes'] = [
+        '#type' => 'fieldset',
+        '#title' => 'Configuration des périodes',
+        '#tree' => TRUE
       ];
-      //
-      $form['jours'][$i]['h_f__m_f'] = [
-        "#type" => 'textfield',
-        '#title' => 'Heure fin',
-        '#default_value' => $val['h_f'] . ':' . $val['m_f']
-      ];
+      // boucles pour les periodes 
+      $periods = $jours[$i]['periodes'];
+      //dump($periods);
+      foreach ($periods as $j => $period) {
+        // dropdown activation
+        $form['jours'][$i]['periodes'][$j] = [
+          "#type" => 'details',
+          '#title' => $period['label'],
+          '#open' => false
+        ];
+        // label
+        $form['jours'][$i]['periodes'][$j]['label'] = [
+          "#type" => 'textfield',
+          '#title' => 'Label',
+          '#default_value' => $period['label']
+        ];
+        // status
+        $form['jours'][$i]['periodes'][$j]['status'] = [
+          "#type" => 'checkbox',
+          '#title' => 'Status',
+          '#default_value' => $period['status']
+        ];
+        // heure de but
+        $form['jours'][$i]['periodes'][$j]['h_d__m_d'] = [
+          "#type" => 'textfield',
+          '#title' => 'Heure debut',
+          '#default_value' => $period['h_d'] . ':' . $period['m_d']
+        ];
+        // heure de but
+        $form['jours'][$i]['periodes'][$j]['h_f__m_f'] = [
+          "#type" => 'textfield',
+          '#title' => 'Heure de fin',
+          '#default_value' => $period['h_f'] . ':' . $period['m_f']
+        ];
+        // decallage : 
+        $form['jours'][$i]['periodes'][$j]['decallage']  = [
+          '#type' => 'number',
+          '#title' => "Valeur de la reduction",
+          '#default_value' => $period['decallage']
+        ];
+        // intervalle : 
+        $form['jours'][$i]['periodes'][$j]['intervalle']  = [
+          '#type' => 'number',
+          '#title' => "Valeur de la reduction",
+          '#default_value' => $period['intervalle']
+        ];
+      }
     }
     $form['reduction'] = [
       '#type' => 'number',
       '#title' => "Valeur de la reduction",
       '#default_value' => $this->config('booking_system.settings')->get('reduction')
     ];
-    $form['months_number'] = [
+    $form['number_of_days'] = [
       '#type' => 'number',
       '#title' => "Nombre de mois à afficher",
-      '#default_value' => $this->config('booking_system.settings')->get('months_number')
+      '#default_value' => $this->config('booking_system.settings')->get('number_of_days')
     ];
     return parent::buildForm($form, $form_state);
   }
@@ -156,10 +145,9 @@ class SettingsForm extends ConfigFormBase implements DaysSettingsInterface {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('booking_system.settings')
-      ->set('booking_type', $form_state->getValue('booking_type'))
       ->set('jours', $form_state->getValue('jours'))
       ->set('reduction', $form_state->getValue('reduction'))
-      ->set('months_number', $form_state->getValue('months_number'))
+      ->set('number_of_days', $form_state->getValue('number_of_days'))
       ->save();
     parent::submitForm($form, $form_state);
   }
